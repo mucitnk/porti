667627bb713aafbb7c968dbaa817d937dbdc2c2a
Munci Tunc <muncitunckalaycioglu@gmail.com>
2022-11-13 14:02:38 +0300

    myportfolio

---
+++ server.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, url_for, request
+import csv
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+	return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+	return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		try:
+			data = request.form.to_dict()
+			write_to_csv(data)
+			return 'form submitted!'
+		except:
+			return 'didnt save database'
+	else:
+		return 'not woyking'
+
+def write_to_file(data):
+	with open('database.txt', mode='a') as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+	with open('database.csv', mode='a') as database2:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+##################################################
+### Alt kisim redundant
+##################################################
+# @app.route('/index.html')
+# def home():
+# 	return render_template('index.html')
+
+# # @app.route('/works.html')
+# # def work():
+# # 	return render_template('works.html')
+
+# @app.route('/about.html')
+# def about():
+# 	return 'Hello, Bolg!'
+
+# @app.route('/project.html')
+# def about():
+# 	return render_template('project.html')
+
+# @app.route('/components')
+# def blog():
+# 	return 'Hello, Bolg!'
+
+# @app.route('/<username>')
+# def about(username=None):
+# 	return render_template('about.html', name=username)
